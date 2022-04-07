c035a103ee1bf62a1a39c232c8edc3d5cc1ffbcb
sisaif56 <85629017+sisaif56@users.noreply.github.com>
2022-04-07 16:20:23 +0600

    reverse

---
+++ Code/Reverse_a_Number/reverse.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+class A
+{
+    int n, r;
+
+public:
+    void input()
+    {
+        cout << "Enter the value : " << endl;
+        cin >> n;
+    }
+    void output()
+    {
+        while (n > 0)
+        {
+            r = n % 10;
+            cout << r;
+            n = n / 10;
+        }
+    }
+};
+
+int main()
+{
+    A obj;
+    obj.input();
+    obj.output();
+    return 0;
+}
